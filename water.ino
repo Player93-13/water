9b3b9b24459a21aeb89e2ba81be088e4de6c85d9
Player93-13 <57127746+Player93-13@users.noreply.github.com>
2021-04-16 18:40:49 +0300

    Initial commit

---
+++ water.ino
@@ -0,0 +1,42 @@
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
+
+const char *ssid     = "HUAWEI-42";
+const char *password = "123asdqwe";
+
+WiFiUDP ntpUDP;
+
+// By default 'pool.ntp.org' is used with 60 seconds update interval and
+// no offset
+NTPClient timeClient(ntpUDP);
+
+// You can specify the time server pool and the offset, (in seconds)
+// additionally you can specify the update interval (in milliseconds).
+// NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+  for(int i = 0; i < 10; i++)
+  {
+    Serial.println(timeClient.getFormattedTime());
+    delay(1000);
+  }
+ 
+  delay(1000);
+}
